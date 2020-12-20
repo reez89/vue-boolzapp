776493585b16e564f9f29e4930438407b705ea1e
Nicola R <nicola.rizzi89@hotmail.it>
2020-12-20 10:28:08 +0100

    Mileston 1 complete
    Added v-for cycle about the user list.

--- milestone1/main1.js
+++ milestone1/main1.js
@@ -1,7 +1,12 @@
 let app = new Vue ({
     el: "#app",
     data:{
-
+        users:[
+            {avatar : 'img/avatar_1.jpg', name: 'Michele'},
+            {avatar : 'img/avatar_2.jpg', name: 'Fabio'},
+            {avatar : 'img/avatar_3.jpg', name: 'Samuele'},
+            {avatar : 'img/avatar_4.jpg', name: 'Luisa'}
+        ],
     }
 
 
